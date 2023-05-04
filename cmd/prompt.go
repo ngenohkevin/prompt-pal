7e6275fe74d7c6622c383ccd54879fb12a5b22b2
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-04 12:32:08 +0300

    integrated chatgpt package

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"github.com/ngenohkevin/prompt-pal.git/pkg/chatgpt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
 )
 
 var promptCmd = &cobra.Command{
@@ -10,13 +15,49 @@ var promptCmd = &cobra.Command{
 	Long: `The prompt command allows you to interact with OpenAI's ChatGPT API. 
 With the prompt command, you can provide a prompt to the API and get a response back.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := cmd.Help()
-		if err != nil {
-			return
-		}
+		executePrompt()
 	},
+}
+
+func executePrompt() {
+	prompt := viper.GetString("prompt")
+	length := viper.GetInt("length")
+	temperature := viper.GetFloat64("temperature")
+
+	chatbot, err := chatgpt.GetChatResponse(prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := chatbot.GenerateResponse(context.Background(), prompt, length, temperature)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(response)
 }
 
 func init() {
 	rootCmd.AddCommand(promptCmd)
+	promptCmd.Flags().StringP("prompt", "p", "", "The prompt to send to ChatGPT")
+	err := viper.BindPFlag("prompt", promptCmd.Flags().Lookup("prompt"))
+	if err != nil {
+		return
+	}
+	err = promptCmd.MarkFlagRequired("prompt")
+	if err != nil {
+		return
+	}
+
+	promptCmd.Flags().IntP("length", "l", 128, "The length of the response to generate")
+	err = viper.BindPFlag("length", promptCmd.Flags().Lookup("length"))
+	if err != nil {
+		return
+	}
+
+	promptCmd.Flags().Float64P("temperature", "t", 0.5, "The temperature to use for response generation")
+	err = viper.BindPFlag("temperature", promptCmd.Flags().Lookup("temperature"))
+	if err != nil {
+		return
+	}
 }
