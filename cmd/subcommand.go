56661539a0ef4a453464afadcf960f43f749a334
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-04 12:33:15 +0300

    integrated chatgpt package

--- cmd/subcommand.go
+++ cmd/subcommand.go
@@ -1,32 +1,52 @@
 package cmd
 
-import (
-	"fmt"
-	"github.com/spf13/cobra"
-)
-
-var generateCmd = &cobra.Command{
-	Use:   "generate",
-	Short: "Generate response to a prompt using GPT-3",
-	Run: func(cmd *cobra.Command, args []string) {
-		prompt, _ := cmd.Flags().GetString("prompt")
-		length, _ := cmd.Flags().GetInt("length")
-		temperature, _ := cmd.Flags().GetFloat64("temperature")
-
-		//	TODO: Implement generating response using GPT-3
-		fmt.Printf("Generating response to prompt: %s\n", prompt)
-		fmt.Printf("Length: %d,  Temperature: %.2f\n", length, temperature)
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(generateCmd)
-
-	generateCmd.Flags().StringP("prompt", "p", "", "prompt for generating usage")
-	err := generateCmd.MarkFlagRequired("prompt")
-	if err != nil {
-		return
-	}
-	generateCmd.Flags().IntP("length", "l", 1024, "Length of response")
-	generateCmd.Flags().Float64P("temperature", "t", 0.7, "Temperature for generating response")
-}
+//import (
+//	"context"
+//	"fmt"
+//	"github.com/ngenohkevin/prompt-pal.git/pkg/chatgpt"
+//	"github.com/spf13/cobra"
+//	"github.com/spf13/viper"
+//	"log"
+//)
+//
+//var subCommandCmd = &cobra.Command{
+//	Use:   "subcommand",
+//	Short: "Interact with the OpenAI Chatbot via a subcommand",
+//	Run: func(cmd *cobra.Command, args []string) {
+//		prompt, _ := cmd.Flags().GetString("prompt")
+//		length, _ := cmd.Flags().GetInt("length")
+//		temperature, _ := cmd.Flags().GetFloat64("temperature")
+//
+//		chatbot, err := chatgpt.GetChatResponse(prompt)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//
+//		response, err := chatbot.GenerateResponse(context.Background(), prompt, length, temperature)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//
+//		fmt.Printf("Response: %s\n", response)
+//	},
+//}
+//
+//func init() {
+//	rootCmd.AddCommand(subCommandCmd)
+//
+//	subCommandCmd.Flags().StringP("prompt", "p", "", "prompt for generating usage")
+//	err := subCommandCmd.MarkFlagRequired("prompt")
+//	if err != nil {
+//		return
+//	}
+//	subCommandCmd.Flags().IntP("length", "l", 128, "Length of response")
+//	subCommandCmd.Flags().Float64P("temperature", "t", 0.7, "Temperature for generating response")
+//	err = viper.BindPFlag("prompt", subCommandCmd.Flags().Lookup("prompt"))
+//	if err != nil {
+//		return
+//	}
+//	err = subCommandCmd.MarkFlagRequired("prompt")
+//	if err != nil {
+//		return
+//	}
+//}
